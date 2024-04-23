a5fe15bc2b094013c38c04d770c5dff0a8d474e0
Koxypo <onemorekorn@gmail.com>
2024-04-23 15:58:41 +0500

    выполнил первое дз

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 'use strict'
 
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
+
+const personalMovieDb = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	private: false,
+}
+
+let oneOfTheLastMovies = prompt('Один из последних фильмов?', '')
+let countOfLastMovie = +prompt('На сколько его оцените?')
+
+personalMovieDb.movies[oneOfTheLastMovies] = countOfLastMovie
+
+let oneOfTheLastMovies1 = prompt('Один из последних фильмов?', '')
+let countOfLastMovie1 = +prompt('На сколько его оцените?')
+
+personalMovieDb.movies[oneOfTheLastMovies1] = countOfLastMovie1
