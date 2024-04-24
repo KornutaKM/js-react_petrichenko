744aab4c92edfce4d578988870ff64d31253cb04
Koxypo <onemorekorn@gmail.com>
2024-04-24 14:32:27 +0500

    все убрали в функции

--- script.js
+++ script.js
@@ -1,6 +1,60 @@
 'use strict'
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
+let numberOfFilms
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 0)
+	}
+}
+
+function rememberMyFilms() {
+	let count = 0
+
+	while (count < 2) {
+		let oneOfTheLastMovies = prompt('Один из последних фильмов?', '')
+		if (
+			oneOfTheLastMovies == '' ||
+			oneOfTheLastMovies == null ||
+			oneOfTheLastMovies.length > 50
+		) {
+			continue
+		}
+		let countOfLastMovie = +prompt('На сколько его оцените?')
+
+		personalMovieDb.movies[oneOfTheLastMovies] = countOfLastMovie
+
+		count++
+	}
+}
+
+function detectPersonalLevel() {
+	if (personalMovieDb.count <= 10) {
+		console.log('Просмотрено довольно мало фильмов')
+	} else if (personalMovieDb.count <= 30) {
+		console.log('Вы классический зритель')
+	} else if (personalMovieDb.count > 30) console.log('Вы киноман')
+	else {
+		console.log('Произошла ошибка')
+	}
+}
+
+function showMyDB() {
+	if (personalMovieDb.private == false) console.log(personalMovieDb)
+}
+
+function writeYourGenres() {
+	for (let i = 0; i < 3; i++) {
+		personalMovieDb.genres[i] = prompt(
+			`Ваш любимый жанр под номером ${i + 1}:`,
+			''
+		)
+	}
+}
+
+start()
 
 const personalMovieDb = {
 	count: numberOfFilms,
@@ -10,29 +64,10 @@ const personalMovieDb = {
 	private: false,
 }
 
-let count = 0
+rememberMyFilms()
 
-while (count < 2) {
-	let oneOfTheLastMovies = prompt('Один из последних фильмов?', '')
-	if (
-		oneOfTheLastMovies == '' ||
-		oneOfTheLastMovies == null ||
-		oneOfTheLastMovies.length > 50
-	) {
-		continue
-	}
-	let countOfLastMovie = +prompt('На сколько его оцените?')
+detectPersonalLevel()
 
-	personalMovieDb.movies[oneOfTheLastMovies] = countOfLastMovie
+showMyDB()
 
-	count++
-}
-
-if (personalMovieDb.count <= 10) {
-	console.log('Просмотрено довольно мало фильмов')
-} else if (personalMovieDb.count <= 30) {
-	console.log('Вы классический зритель')
-} else if (personalMovieDb.count > 30) console.log('Вы киноман')
-else {
-	console.log('Произошла ошибка')
-}
+writeYourGenres()
